ecd7f5e77bb76d85017d482832c3082551dbbe8e
alexis-jacq <alexis.jacq@gmail.com>
2017-04-13 21:03:27 +0100

    setup.py for signal_learner

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import os
 from distutils.core import setup
 import glob
 
-setup(name='',
+setup(name='signalLearner',
       version='0.0',
       license='ISC',
-      description='',
+      description='RL agents learning actions from signals',
       author='Alexis Jacq',
       author_email='alexis.jacq@gmail.com',
       package_dir = {'': 'src'},
-      packages=[''],
+      packages=['signal_learner'],
       data_files=[('share/doc/', ['AUTHORS', 'LICENSE', 'README.md'])]
       )
