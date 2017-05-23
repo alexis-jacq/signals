1b3066945f18dfaa9d34fb3a042b1113eee533e2
alexis-jacq <alexis.jacq@gmail.com>
2017-05-23 12:33:35 +0100

    updatin package description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import glob
 setup(name='signalLearner',
       version='0.0',
       license='ISC',
-      description='RL agents learning actions from signals',
+      description='agents recognising and learn actions from human body signals',
       author='Alexis Jacq',
       author_email='alexis.jacq@gmail.com',
       package_dir = {'': 'src'},
